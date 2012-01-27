9234cd45c31ae18da552d497da5b93a72eb5704f
Mukasa Patrick <touchwire@gmail.com>
2012-01-26 18:13:18 -0800

    Basic stylesheet in place -- ubuntu web fonts added

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,6 +2,18 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+	/* view helpers */
+	function __initView(){
+		
+		$this->bootstrap('layout');
+		$layout = $this->getResource('layout');
+		$view = $layout->getView();
+		
+		$view->doctype('HTML4_STRICT');
+		$view->HeadMeta()->appendEquiv('content-type', 'text/html;chartset=UTF-8');
+		$view->headTitle('My Site')->setSeparator(' :: ');
+		
+	}
 
 
 }
