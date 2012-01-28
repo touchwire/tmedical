187ff9becf58bca616e7f74be1e0308c7b65d9ba
Mukasa Patrick <touchwire@gmail.com>
2012-01-27 20:20:30 -0800

    User Module Created - Controllers and actions added

---
+++ application/modules/users/controllers/AdminController.php
@@ -0,0 +1,46 @@
+<?php
+
+class Users_AdminController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function createAction()
+    {
+        // action body
+    }
+
+    public function updateAction()
+    {
+        // action body
+    }
+
+    public function deleteAction()
+    {
+        // action body
+    }
+
+    public function listAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
